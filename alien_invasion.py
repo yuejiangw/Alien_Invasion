2ceed9d3291f5d1a667317075274fe5e823af74a
wyj226 <308033240@qq.com>
2019-05-31 19:13:08 +0800

    新增setting类，重构了alien_invasino.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,10 +1,14 @@
 import sys
 import pygame
+from settings import Settings 
 
 def run_game():
-    # 初始化游戏并创建一个屏幕对象
+    # 初始化pygame、设置和屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200, 600))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height)
+    )
     pygame.display.set_caption("Alien Invasion")
 
     # 设置背景颜色
@@ -19,7 +23,7 @@ def run_game():
                 sys.exit()
         
         # 每次循环时都重绘屏幕
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
