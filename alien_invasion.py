37f16ec8171f8f0ab9c1e00dc59e1aef2e04b2b4
wyj226 <308033240@qq.com>
2019-05-31 22:40:28 +0800

    以行为单位新增外星人

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,6 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings 
 from ship import Ship
-from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -21,8 +20,11 @@ def run_game():
     # 创建一个用于存储子弹的编组
     bullets = Group()
 
-    # 创建一个外星人
-    alien = Alien(ai_settings, screen)
+    # 创建一个外星人编组
+    aliens = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, aliens)
 
     # 开始游戏主循环
     while True:
@@ -37,6 +39,6 @@ def run_game():
         gf.update_bullets(bullets)
 
         # 更改屏幕上的图像并切换到新的屏幕
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
