d1d31d9e6e0f6f03f048a0ab48f6ff2ab3993b6c
wyj226 <308033240@qq.com>
2019-05-31 20:09:34 +0800

    飞船速度可以调整，增加边界检测

--- game_functions.py
+++ game_functions.py
@@ -9,9 +9,13 @@ def check_events(ship):
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT or event.key == pygame.K_d:  # 飞船右移
                 ship.moving_right = True
+            elif event.key == pygame.K_LEFT or event.key == pygame.K_a: # 飞船左移
+                ship.moving_left = True
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                 ship.moving_right = False
+            elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                ship.moving_left = False
 
 def update_screen(ai_settings, screen, ship):
     """更新屏幕上的图像，并切换到新屏幕"""
