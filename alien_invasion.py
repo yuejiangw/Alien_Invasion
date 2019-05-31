35a9be7c3d04cb523f3a668b3ff4c282a2980047
wyj226 <308033240@qq.com>
2019-05-31 21:21:40 +0800

    新增子弹发射功能，空格发射

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from pygame.sprite import Group
 from settings import Settings 
 from ship import Ship
 import game_functions as gf
@@ -16,16 +17,22 @@ def run_game():
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
 
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
+
     # 开始游戏主循环
     while True:
 
         # 监视键盘和鼠标事件
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         
         # 飞船移动
         ship.update()
 
+        # 子弹移动
+        bullets.update()
+        
         # 更改屏幕上的图像并切换到新的屏幕
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
