c1f84031bdcad5d11eaece9ef300a169c786caff
wyj226 <308033240@qq.com>
2019-06-01 10:16:04 +0800

    重构gamefunction，将碰撞检测与更新子弹拆分开

--- game_functions.py
+++ game_functions.py
@@ -59,6 +59,9 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     # 检查是否有子弹击中了外星人，如果有则删除相应的外星人
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
     if len(aliens) == 0:
