a33abf051c76ab2b9d7f62be7980d4eff25642a7
226wyj <308033240@qq.com>
2019-06-06 15:47:57 +0800

    276

--- game_functions.py
+++ game_functions.py
@@ -100,6 +100,7 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, 
         for aliens in collisions.values():
             stats.score += ai_settings.alien_points * len(aliens)
             sb.prep_score()
+        check_high_score(stats, sb)
     if len(aliens) == 0:
         # 删除现有的子弹，加快游戏节奏，并新建一群外星人
         bullets.empty()
@@ -195,3 +196,9 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
 
     # 检查是否有外星人到达屏幕底端
     check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
+
+def check_high_score(stats, sb):
+    """检查是否诞生了新的最高分"""
+    if stats.score > stats.high_score:
+        stats.high_score = stats.score
+        sb.prep_high_score()
--- scoreboard.py
+++ scoreboard.py
@@ -14,12 +14,15 @@ class Scoreboard():
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
 
-        # 准备初始得分图像
+        # 准备包含最高得分和当前得分的图像
         self.prep_score()
+        self.prep_high_score()
+        self.prep_level()
 
     def prep_score(self):
         """将得分转换为一幅渲染的图像"""
-        score_str = str(self.stats.score)
+        rounded_score = int(round(self.stats.score, -1))
+        score_str = "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True, self.text_color, self.ai_settings.bg_color)
 
         # 将得分放在屏幕右上角
@@ -28,6 +31,30 @@ class Scoreboard():
         self.score_rect.top = 20
 
     def show_score(self):
-        """在屏幕上显示得分"""
+        """在屏幕上显示飞船和得分"""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
+
+    def prep_high_score(self):
+        """将最高得分转换为渲染的图像"""
+        high_score = int(round(self.stats.high_score, -1))
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.ai_settings.bg_color)
+
+        # 将最高得分放在屏幕顶部中央
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.screen_rect.top
+
+    def prep_level(self):
+        """将等级转换为渲染的图像"""
+        self.level_image = self.font.render(str(self.stats.level), True,
+            self.text_color, self.ai_settings.bg_color)
+        
+        # 将等级放在得分下方
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+    
         
